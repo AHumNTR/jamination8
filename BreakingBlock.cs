a2f2676d2f2b7ddf3604b8d7380c826280833df1
HumN <ahumantr@gmail.com>
2025-10-18 12:02:20 +0300

    breakingblock

--- BreakingBlock.cs
+++ BreakingBlock.cs
@@ -4,18 +4,46 @@ using System;
 public partial class BreakingBlock : RigidBody2D
 {
 	// Called when the node enters the scene tree for the first time.
+	[Export]
+	public Texture2D[] textures;
 	Sprite2D sprite;
-	CollisionObject2D col;
+	CollisionShape2D col;
+	Timer timer;
+	int stage=0;
 	public override void _Ready()
     {
 		BodyEntered += bodyEnter;
 		sprite = GetNode<Sprite2D>("Sprite2D");
-		col = GetNode<CollisionObject2D>("CollisionObject2D");
+		col = GetNode<CollisionShape2D>("CollisionShape2D");
+		timer = GetNode<Timer>("Timer");
+		timer.Timeout += timeout;
     }
-
-public void bodyEnter(Node body)
+	public void timeout()
+	{
+		if (GetCollidingBodies().Count > 0)
+		{
+			if (stage < 2)
+			{
+				timer.Start();
+			}
+			else
+			{
+				col.Disabled = true;
+				sprite.Visible = false;
+			}
+		}
+		else
+		{
+			sprite.Visible = true;
+			col.Disabled = false;
+			stage = 0;
+		}
+		sprite.Texture = textures[stage];
+		stage++;
+    }
+	public void bodyEnter(Node body)
     {
-		
+		timer.Start();			
     }
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
