51ce5e9f43f12308f2b0e1017e75917ed382ea15
HumN <ahumantr@gmail.com>
2025-10-18 04:02:41 +0300

    added mechanics

--- Player.cs
+++ Player.cs
@@ -3,15 +3,27 @@ using System;
 
 public partial class Player : RigidBody2D
 {
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
+	[Export]
+	public float jumpForce;
+
+	int swingDirection = 1;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		Rotate((float)delta * swingDirection);
+		if (Input.IsActionJustPressed("ChangeRotation"))
+		{
+			swingDirection = swingDirection * -1;
+		}
+		if (Input.IsActionJustPressed("Jump"))
+		{
+			ApplyForce(-Transform.Y * jumpForce);
+		}
+		if (Math.Abs(RotationDegrees%360) > 60)
+		{
+			GD.Print("ov");
+		}
 	}
+
 }
